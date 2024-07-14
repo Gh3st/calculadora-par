9a025586742dfcb3b3361aecc0eddb61a1ef7d6d
natin <natin@OVVQX507>
2024-07-13 22:03:58 -0300

    Adicionar arquivos de projeto.

---
+++ par ou impa.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("digite um numero para saber se ele é divisivel por 2");
+int numero = 1;
+numero = Convert.ToInt32(Console.ReadLine());
+
+if (numero % 2 == 0)
+{
+    Console.WriteLine("par");
+}
+else
+{
+    Console.WriteLine("impa");
+}
